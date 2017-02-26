5286071ff6eb0be3fe36c12a2bb17ee01763a45a
Christof Vollrath <christof@taobits.net>
2017-02-26 17:37:37 +0100

    Initial checkin of Kata

---
+++ src/test/java/org/codecop/lcdnumbers/PatternsTest.java
@@ -0,0 +1,20 @@
+package org.codecop.lcdnumbers;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class PatternsTest {
+    
+    @Test
+    public void shouldKnowLinesOfOne() {
+        Patterns patterns = new Patterns();
+        assertEquals(Arrays.asList(new Line("   "), //
+                                   new Line("  |"), //
+                                   new Line("   "), //
+                                   new Line("  |"), //
+                                   new Line("   ")), patterns.of(1));
+    }
+}
